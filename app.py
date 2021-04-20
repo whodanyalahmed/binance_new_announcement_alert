e370dbf0c8e35593270bd9496304138e556ced15
whodanyalahmed <daniahmedkhatri@gmail.com>
2021-04-20 13:01:12 +0500

    complete

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from requests import get
-import time,winsound
-
+import time,chime
 from bs4 import BeautifulSoup
 url = "https://www.binance.com/en/support/announcement/c-48"
+# url = "https://www.binance.com/en/support/announcement/c-49?navId=49"
 
 print("info : processing")
-timeint = int(input("Enter time interval : "))
+timeint = int(input("Enter time interval (in sec) : "))
 def getLatestNews(url):    
     newList = []
     text = get(url).text
@@ -22,7 +22,9 @@ def writeToFile(filename,announcements):
     for ele in announcements:
         f.write(ele+'\n')
     print("info : wrote to file")
-writeToFile(filename,announcements)
+
+# writeToFile(filename,announcements)
+
 while True:
     # d = input("wait ? y/n")
     # if(d == "y"):
@@ -34,18 +36,40 @@ while True:
     li = f.read()
     li = li.split('\n')[:-1]
     # print(li)
+    li_set = set(li)
+    announcements_set = set(announcements)
     if(len(li) == len(announcements)):
+      
         if(li == announcements):
             print("info : there is no new post")
         else:
             print("info : there is a new post in announcements\a")
-            for i in range(10):
-                print("\a")
-                time.sleep(1)
-            
-
+            for i in range(5):
+                chime.success()   
+            #     print("\a")
+                time.sleep(1)     
+            new_posts = announcements_set.difference(li_set)
+            new_posts =list(new_posts)
+            print("\n======================")
+            print("These are new posts: ")
+            for new_post in new_posts:
+                print( "=> " + new_post) 
+            print("======================\n")
             writeToFile(filename,announcements)
     else:
         print("info : both have different lenghts")
+        print("info : there is a new post in announcements\a")
+        for i in range(5):
+            chime.success()   
+        #     print("\a")
+            time.sleep(1)     
+        new_posts = announcements_set.difference(li_set)
+        new_posts = list(new_posts)
+        print("\n======================")
+        print("These are new posts: ")
+        for new_post in new_posts:
+            print( "=> " + new_post) 
+        print("======================\n")
         writeToFile(filename,announcements)
+    
     time.sleep(timeint)
